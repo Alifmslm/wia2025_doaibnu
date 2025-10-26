b5c4a757989f19674ab3705e2ffe8784bd3a7da4
Alifmslm <alifmslm01@gmail.com>
2025-10-26 12:45:02 +0700

    feat: login header build

---
+++ src/component/HeaderAuth.tsx
@@ -0,0 +1,14 @@
+import "../style/HeaderAuth.css";
+
+function HeaderAuth() {
+    return (
+        <>
+            <header className="header_auth">
+                <h1 className="header_auth_header">Selamat Datang <br></br> Kembali 👋</h1>
+                <p className="header_auth_subheader">Masuk untuk melanjutkan eksplorasimu bersama pelaku UMKM hebat!</p>
+            </header>
+        </>
+    )
+}
+
+export default HeaderAuth
--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -1,10 +1,15 @@
 import Form from "../component/Form.tsx";
 import Button from "../component/Button.tsx";
+import HeaderAuth from "../component/HeaderAuth.tsx";
+import "../style/LoginPage.css";
+import LogoWia from "../assets/logo_wia.jpg";
 
 function LoginPage() {
     return(
         <>
-            <section>
+            <section className="container_login">
+                <img src={LogoWia} alt="" className="img_logo"/>
+                <HeaderAuth/>
                 <Form/>
                 <Button/>
             </section>
