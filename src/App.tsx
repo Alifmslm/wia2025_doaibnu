1ca021398790d9d1b4b0fb38f9925dec24991d88
Alifmslm <alifmslm01@gmail.com>
2025-10-26 19:45:24 +0700

    feat: add routing feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './style/App.css'
 import RegisterPage from './page/RegisterPage.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from './page/LoginPage.tsx';
 
 function App() {
   return (
-    <>
-      <RegisterPage/>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/register' element={<RegisterPage/>}></Route>
+          <Route path='/login' element={<LoginPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
