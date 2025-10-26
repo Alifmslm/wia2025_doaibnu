64cdc8fa9d5b58c2627eed4a5b1461fea3951412
Alifmslm <alifmslm01@gmail.com>
2025-10-26 14:32:46 +0700

    feat: login screen - desktop

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -1,5 +1,6 @@
 import Form from "../component/Form.tsx";
 import HeaderAuth from "../component/HeaderAuth.tsx";
+import SideImage from "../component/SideImage.tsx";
 import "../style/LoginPage.css";
 import LogoWia from "../assets/logo_wia.jpg";
 
@@ -7,6 +8,7 @@ function LoginPage() {
     return (
         <>
             <form className="auth_form">
+                <SideImage/>
                 <section className="container_login">
                     <img src={LogoWia} alt="" className="img_logo" />
                     <HeaderAuth />
