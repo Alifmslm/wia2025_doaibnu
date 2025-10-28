e7f4fcc1af70efa95ee5fe50136354076cfd80ab
Alifmslm <alifmslm01@gmail.com>
2025-10-28 15:47:27 +0700

    add: header default for home

--- src/ui/component/macro-components/HeaderDefault.tsx
+++ src/ui/component/macro-components/HeaderDefault.tsx
@@ -1,7 +1,17 @@
+import "../../../style/HeaderDefault.css";
+import IconCircular from "../micro-components/IconCircular.tsx";
+import LogoWia from "../../../assets/logo_wia.jpg";
+
 function HeaderDefault() {
     return(
         <>
-            
+            <header className="header-home">
+                <img src={LogoWia} alt="logo-web" className="logo-home" />
+                <nav className="icon-group">
+                    <IconCircular />
+                    <IconCircular />
+                </nav>
+            </header>
         </>
     )
 }
