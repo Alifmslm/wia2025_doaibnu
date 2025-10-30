c625907c8cc21664f51e55a427ba599ca57df493
Alifmslm <alifmslm01@gmail.com>
2025-10-30 09:09:09 +0700

    feat: gallery page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../ui/page/LoginPage.tsx";
 import HomePage from "../ui/page/HomePage.tsx";
 import DetailPage from "../ui/page/DetailPage.tsx";
+import GalleryPage from "../ui/page/GalleryPage.tsx";
 
 function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ function AppRoutes() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/detail-page" element={<DetailPage />} />
+            <Route path="/gallery-page-" element={<GalleryPage />} />
         </Routes>
     );
 }
---
+++ src/ui/page/GalleryPage.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "../../style/GalleryPage.css";
+import CloseButton from "../../assets/close-button.png";
+import GalleryImage1 from "../../assets/gallery-image-1.png";
+import GalleryImage2 from "../../assets/gallery-image-2.png";
+import GalleryImage3 from "../../assets/gallery-image-3.png";
+import GalleryImage4 from "../../assets/gallery-image-4.png";
+
+function GalleryPage() {
+    const images = [GalleryImage1, GalleryImage2, GalleryImage3, GalleryImage4];
+    const [selectedImage, setSelectedImage] = useState(images[0]); // default gambar pertama
+
+    return (
+        <>
+            <section className="gallery-page">
+                {/* Tombol Close */}
+                <nav className="icon-group">
+                    <div className="icon-item">
+                        <img src={CloseButton} alt="close-button" />
+                    </div>
+                </nav>
+
+                {/* Gambar utama */}
+                <img src={selectedImage} alt="Main" className="img-main" />
+
+                {/* Carousel bawah */}
+                <section className="gallery-carousel">
+                    <div className="carousel-header">
+                        <h1>Soto Ayam Bu Carti</h1>
+                        <h1>{images.indexOf(selectedImage) + 1}/{images.length}</h1>
+                    </div>
+
+                    <div className="carousell-list">
+                        {images.map((img, index) => (
+                            <div
+                                key={index}
+                                className={`carousel-item-wrapper ${
+                                    selectedImage === img ? "active" : ""
+                                }`}
+                                onClick={() => setSelectedImage(img)}
+                            >
+                                <img src={img} alt={`thumb-${index}`} className="carousel-item" />
+                                {/* Overlay hanya muncul jika gambar tidak aktif */}
+                                {selectedImage !== img && (
+                                    <div className="carousel-overlay"></div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </section>
+        </>
+    );
+}
+
+export default GalleryPage;
