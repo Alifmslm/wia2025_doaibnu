b0dbeec761421cf6a663f591987bb6f8400a5631
Alifmslm <alifmslm01@gmail.com>
2025-11-07 21:20:30 +0700

    error: hero decoration banner

--- src/ui/page/LandingPage/HeroDecoration.tsx
+++ src/ui/page/LandingPage/HeroDecoration.tsx
@@ -1,8 +1,24 @@
+import StarDecor from "../../../assets/star_decoration.png";
+
+const BannerContent = () => (
+    <>
+        <p>DUKUNG UMKM DISEKITARMU</p>
+        <img alt="decor-icon" className="separator-icon" src={StarDecor} />
+    </>
+);
+
 function HeroDecoration() {
     return (
-        <>
-            
-        </>
+        <div className="hero-decoration-wrapper">
+            <div className="banner banner-green">
+                <BannerContent />
+                <BannerContent />
+            </div>
+            <div className="banner banner-orange">
+                <BannerContent />
+                <BannerContent />
+            </div>
+        </div>
     )
 }
 
