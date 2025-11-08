317d43222ffa222acb2a9cc3417066ddea419e6a
Alifmslm <alifmslm01@gmail.com>
2025-11-08 08:44:12 +0700

    feat: mobile vers footer

--- src/ui/page/LandingPage/FooterLanding.tsx
+++ src/ui/page/LandingPage/FooterLanding.tsx
@@ -1,15 +1,19 @@
 import LogoWhite from '../../../assets/logo-white.png';
+import BgFooter from '../../../assets/bg-footer.png';
 
 function FooterLanding() {
     return (
         <>
             <footer className='footer-landing'>
-                <div className="footer-main">
-                    <img src={LogoWhite} alt="" />
-                    <h1>Dukung UMKM Lokal, Mulai dari Sekitar Kita.</h1>
-                    <p>Platform direktori digital dimana anda bisa mengeksplor umkm sekitar</p>
+                <div className="footer-wrapper">
+                    <div className="footer-main">
+                        <img src={LogoWhite} alt="" />
+                        <h1>Dukung UMKM Lokal, Mulai dari Sekitar Kita.</h1>
+                        <p>Platform direktori digital dimana anda bisa mengeksplor umkm sekitar</p>
+                    </div>
+                    <p className='copyright'>Made by Doa Ibnu Tim, 2025.</p>
                 </div>
-                <p className='copyright'>Made by Doa Ibnu Tim, 2025.</p>
+                <img src={BgFooter} alt="" className='bg-footer' />
             </footer>
         </>
     );
