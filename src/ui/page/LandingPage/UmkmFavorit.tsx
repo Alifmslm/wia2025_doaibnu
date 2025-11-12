785d87460cb9f28995d2940863873e81670a5727
Alifmslm <alifmslm01@gmail.com>
2025-11-12 17:32:01 +0700

    feat: next and prev button work functionaly in landing page

--- src/ui/page/LandingPage/UmkmFavorit.tsx
+++ src/ui/page/LandingPage/UmkmFavorit.tsx
@@ -1,7 +1,26 @@
 import UmkmFavoritCard from "./UmkmFavoritCard";
 import "../../../style/UmkmFavorit.css";
+import { useRef } from 'react'; // 1. Import useRef
 
 function UmkmFavorit() {
+    // 2. Buat ref untuk container carousel
+    const carouselRef = useRef<HTMLDivElement>(null);
+
+    const scrollAmount = 300; 
+
+    // 3. Buat fungsi untuk handle klik
+    const handlePrev = () => {
+        if (carouselRef.current) {
+            carouselRef.current.scrollLeft -= scrollAmount;
+        }
+    };
+
+    const handleNext = () => {
+        if (carouselRef.current) {
+            carouselRef.current.scrollLeft += scrollAmount;
+        }
+    };
+
     return (
         <>
             <section className="umkm-favorit">
@@ -10,7 +29,8 @@ function UmkmFavorit() {
                     <p>Inilah daftar usaha yang paling diminati dan direkomendasikan oleh pelanggan di sekitar Anda.</p>
                 </div>
                 <div className="outer">
-                    <div className="umkm-favorit__carousell">
+                    {/* 4. Lampirkan ref ke elemen carousel */}
+                    <div className="umkm-favorit__carousell" ref={carouselRef}> 
                         <UmkmFavoritCard isOff={false} />
                         <UmkmFavoritCard isOff={true} />
                         <UmkmFavoritCard isOff={true} />
@@ -23,8 +43,13 @@ function UmkmFavorit() {
                     </div>
                 </div>
                 <div className="umkm-favorit__button">
-                    <button className="carousel-btn btn-prev"><i className="fa-solid fa-chevron-left"></i></button>
-                    <button className="carousel-btn btn-next"><i className="fa-solid fa-chevron-right"></i></button>
+                    {/* 5. Tambahkan onClick ke tombol */}
+                    <button className="carousel-btn btn-prev" onClick={handlePrev}>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </button>
+                    <button className="carousel-btn btn-next" onClick={handleNext}>
+                        <i className="fa-solid fa-chevron-right"></i>
+                    </button>
                 </div>
             </section>
         </>
