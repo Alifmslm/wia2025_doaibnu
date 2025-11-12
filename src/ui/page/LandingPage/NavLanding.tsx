e8b8eeed3b9452dad5fb14aeafc9894f938be9a7
Alifmslm <alifmslm01@gmail.com>
2025-11-12 20:08:53 +0700

    feat: enhance navigation menu with toggle functionality and styling

--- src/ui/page/LandingPage/NavLanding.tsx
+++ src/ui/page/LandingPage/NavLanding.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import Logo from '../../../assets/logo.png';
 
 function NavLanding() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="nav-landing">
-            <img src={Logo} alt="logo" className="nav-img"/>
-            <ul className="nav-links">
+            <img src={Logo} alt="logo" className="nav-img" />
+            
+            <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
                 <li><a href="/register" className='register-button'>Daftar Sekarang</a></li>
                 <li><a href="/login" className='login-button'>Masuk ke Akun Anda</a></li>
             </ul>
-            <i className="fa-solid fa-bars icon-bar" ></i>
+            
+            <i 
+                className="fa-solid fa-bars icon-bar"
+                onClick={toggleMenu} 
+            ></i>
         </nav>
     );
 }
