ecbde010bded34da3f4419110817d01deee453f0
Alifmslm <alifmslm01@gmail.com>
2025-11-14 22:54:00 +0700

    add: animation to banner in landing page

--- src/ui/page/LandingPage/HeroDecoration.tsx
+++ src/ui/page/LandingPage/HeroDecoration.tsx
@@ -7,30 +7,35 @@ const BannerContent = () => (
     </>
 );
 
+const BannerTrackItems = () => (
+    <>
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+        <BannerContent />
+    </>
+);
+
 function HeroDecoration() {
     return (
         <div className="hero-decoration-wrapper">
             <div className="banner banner-green">
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
+                <div className="banner-track">
+                    <BannerTrackItems /> {/* Set 1 */}
+                    <BannerTrackItems /> {/* Set 2 (Duplikat untuk looping) */}
+                </div>
             </div>
             <div className="banner banner-orange">
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
-                <BannerContent />
+                {/* Menambahkan class 'track-reverse' untuk animasi berlawanan arah */}
+                <div className="banner-track track-reverse">
+                    <BannerTrackItems /> {/* Set 1 */}
+                    <BannerTrackItems /> {/* Set 2 (Duplikat untuk looping) */}
+                </div>
             </div>
         </div>
     )
